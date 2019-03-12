7b3a3bc9dcafa808263f9cfad98b47a78ee74513
Jeffrey Guerra <jeffreyguerra@csu.fullerton.edu>
2019-03-12 11:32:15 -0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, jsonify, abort, render_template, flash, redirect, url_for, session, request, logging
+from flask import Flask, jsonify, request
 
 import sqlite3
+import time
 
 
 CREATE_DB_FILE = 'createdb.sql'
@@ -11,21 +12,18 @@ app = Flask(__name__)
 
 
 @app.cli.command('createdb')
-def create_db():
+#basicauth
+def createdb():
     conn = sqlite3.connect(DATABASE)
     cursor = conn.cursor()
     with open(CREATE_DB_FILE) as queryfile:
         cursor.executescript(queryfile.read())
     conn.close()
 
-@app.route('/')
-def hello_world():
-    return jsonify({"key":"yolo"})
-    return abort(404)
-
 
 #Add tags for a new URL
 @app.route('/article/<url>/tags/<tag>', methods=['PUT', 'DELETE'])
+#basicauth
 def add_url_and_tag(url, tag):
     if request.method == 'PUT':
         try:
@@ -113,95 +111,109 @@ def get_urls_with_given_tag(tag):
     print(query)
     return jsonify({"urls": output}), 200
 
-# #############################Start of comments.. Post new comment on an article#####
-# @app.route('/article/<url>/comments', methods = ['POST'])
-# def post_comment(url):
-#     try:
-#         conn = sqlite3.connect(DATABASE)
-#         cursor = conn.cursor()
-#         query = '''SELECT 1
-#               FROM articles
-#               WHERE id = ?'''
-#         result = cursor.execute(query, (url)).fetchall()
-#
-#
-#         if not result:
-#             return jsonify({"Error": "Article with url doesnt exist"}), 409
-#         #Above is checking if comment can even be made, che cking if article exists
-#         #Now post new comment
-#
-#         query = '''INSERT INTO TABLE(comments,([id?]) )'''
-#
-#         conn.close()
-#         print(query)
-#         return jsonify({"true": "success"}), 200
-#     except Exception as exc:
-#         print(exc)
-#         return jsonify({"false": "failure"}), 400
-#
-# # CREATE TABLE comments(
-# #     id INTEGER PRIMARY KEY AUTOINCREMENT,
-# #     date_added INTEGER NOT NULL,
-# #     author TEXT NOT NULL
-# # );
-#
-# @app.route('/article/<url>/comments/<id>', methods = ['DELETE'])
-# def delete_Comment(url, id):
-#     # Delete an individual comment
-#     conn = sqlite3.connect(DATABASE)
-#     cursor = conn.cursor()
-#     query = '''DELETE FROM comments
-#                 WHERE id = ?'''
-#     cursor.execute(query, (id))
-#
-#     conn.commit()
-#     conn.close()
-# ####Get number of comments on a given article
-# @app.route('/article/<id>/comments/counts', methods = ['GET'])
-# def get_number_of_comments(id):
-#
-#     conn = sqlite3.connect(DATABASE)
-#     conn.row_factory = sqlite3.Row
-#     cursor = conn.cursor()
-#
-#     query = '''SELECT COUNT(*) AS COUNT
-#         FROM comments
-#         WHERE id = ?
-#         GROUP BY id'''
-#
-#
-#     cursor.execute(query, [id])
-#
-#     results = cursor.fetchall()
-#     results = [dict(row) for row in results] ##??
-#     print(type(results))
-#     print(results)
-#
-#     count = results['COUNT']
-#     conn.close()
-#
-#     print(query)
-#     return jsonify({"count": count}), 200 #???
-#
-# ####GET n most recent comments on a URL
-# @app.route('/article/<id>/comments/recent')
-# def get_number_most_recent_comments(id): ###??
-#     conn = sqlite3.connect(DATABASE)
-#     conn.row_factory = sqlite3.Row
-#     cursor = conn.cursor()
-#
-#     query = '''SELECT comment
-#         FROM comments
-#         WHERE id = ?
-#         '''
-#     cursor.execute(query, [id])
-#
-#     results = cursor.fetchall()
-#     results = [dict(row) for row in results]
-#
-#     output = []
-#
-#     for item in results:
-#         output.append(item['comment'])
-#
-#     return jsonify({'comments': output}), 200
+@app.route('/article/<url>/comments', methods = ['POST'])
+def post_comment(url):
+    try:
+        conn = sqlite3.connect(DATABASE)
+        cursor = conn.cursor()
+        query = '''SELECT 1
+              FROM articles
+              WHERE id = ?'''
+        result = cursor.execute(query, (url)).fetchall()
+
+
+        if not result:
+            return jsonify({"Error": "Article with url doesnt exist"}), 409
+        #Above is checking if comment can even be made, che cking if article exists
+        #Now post new comment
+
+        query = '''INSERT INTO comments (url, date_added, author, comment) VALUES (?, ?, ?, ?)'''
+        epoch_time = int(time.time())
+        if 'username' in request.headers:
+            author = request.headers['username']
+        else:
+            author = 'Anon'
+
+        data = request.get_json()
+        if 'comment' in data:
+            comment = data['comment']
+        else:
+            return jsonify({"Error": "No comment"}), 400
+
+        cursor.execute(query, (url, epoch_time, author, comment))
+        conn.commit()
+
+        id = cursor.lastrowid
+
+        conn.close()
+
+        return jsonify({"id": id}), 200
+    except Exception as exc:
+        print(exc)
+        return jsonify({"false": "failure"}), 400
+
+@app.route('/article/<url>/comments/<id>', methods = ['DELETE'])
+def delete_Comment(url, id):
+    # Delete an individual comment
+    conn = sqlite3.connect(DATABASE)
+    cursor = conn.cursor()
+    try:
+        query = '''DELETE FROM comments
+                    WHERE id = ?
+                    AND url = ?'''
+        cursor.execute(query, (id, url))
+
+        conn.commit()
+    except:
+        return jsonify({"Error": "Comment didnt exist"}), 404
+    conn.close()
+    return jsonify({"success": "Comment deleted"}), 200
+
+####Get number of comments on a given article
+@app.route('/article/<id>/comments/count', methods = ['GET'])
+def get_number_of_comments(id):
+    conn = sqlite3.connect(DATABASE)
+    conn.row_factory = sqlite3.Row
+    cursor = conn.cursor()
+
+    query = '''SELECT COUNT(*) AS COUNT
+        FROM comments
+        WHERE id = ?
+        '''
+
+    cursor.execute(query, [id])
+
+    results = cursor.fetchone()
+    results = dict(results)
+    count = results['COUNT']
+    conn.close()
+
+    print(query)
+    return jsonify({"count": count}), 200
+
+####GET n most recent comments on a URL
+@app.route('/article/<url>/comments', methods = ['GET'])
+def get_number_most_recent_comments(url):
+
+    limit = request.args.get('limit')
+    if not limit:
+        return jsonify({"Error": "Please provide limit in query string e.g. ?limit=10"}), 200
+
+    conn = sqlite3.connect(DATABASE)
+    conn.row_factory = sqlite3.Row
+    cursor = conn.cursor()
+
+    query = '''SELECT id, url, date_added, author, comment 
+                FROM comments 
+                WHERE url = ?
+                ORDER BY ID ASC
+                LIMIT ?;'''
+    cursor.execute(query, [url, limit])
+
+    results = cursor.fetchall()
+    results = [dict(row) for row in results]
+
+    for item in results:
+        item['date_added'] = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(item['date_added']))
+
+    return jsonify({'comments': results}), 200
--- startup.sh
+++ startup.sh
@@ -1,5 +1,4 @@
 export FLASK_APP=app.py
 export FLASK_ENV=development
-flask createdb
 
 flask run
